568ca1af1648a44258d5c15929d9710a4b28aaa7
sergey <sergeyloginov067@gmail.com>
2022-07-02 12:21:17 +0300

    test jenkins

--- src/main/java/app/main/Application.java
+++ src/main/java/app/main/Application.java
@@ -3,6 +3,8 @@ package app.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +12,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "app.entity")
 @EnableJpaRepositories(basePackages = "app.dao")
 @ComponentScan(basePackages = "app")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
 }
